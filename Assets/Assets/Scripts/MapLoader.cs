60a045d5c3abaa85cef10a3c12dd0bcf497e15eb
Kai Yasko <yasko.kai@gmail.com>
2025-09-14 20:22:31 -0400

    File import fixes

--- Assets/Assets/Scripts/MapLoader.cs
+++ Assets/Assets/Scripts/MapLoader.cs
@@ -1,35 +1,34 @@
 using UnityEngine;
-using System;
 using System.IO;
 
 public struct NodeData
 {
 	// Node index
-	public byte id;
+	public byte Id;
 
 	// Background texture index
-	public byte background;
+	public byte Background;
 
 	// Number of connected edges
 	public byte EdgeCount;
 
 	// Edge reference indices
-	public byte[] edges;
+	public byte[] Edges;
 
 	// Node name
-	public string label;
+	public string Label;
 }
 
 public struct EdgeData
 {
 	// Edge index
-	public byte id;	
+	public byte Id;	
 
 	// Connected node references
-	public byte[] nodes; 
+	public byte[] Nodes; 
 
 	// UI Button positions, relative to room size(800x600)
-	public Vector2[] points;
+	public Vector2[] Points;
 }
 
 public class MapLoader : MonoBehaviour
@@ -40,18 +39,31 @@ public class MapLoader : MonoBehaviour
 	public NodeData[] nodes = new NodeData[byte.MaxValue];
 	public EdgeData[] edges = new EdgeData[byte.MaxValue];
 
-	private delegate void ParseLineFn(string key, string val, ref byte currId);
-	private ParseLineFn[] parseFns;
+	private delegate void _parseLineFn(string key, string val, ref byte currId);
+	private _parseLineFn[] _parseFns;
 
     void Start()
     {
-		parseFns = new ParseLineFn[] { ParseLineNodes, ParseLineEdges };
+		for(byte i = 0; i < byte.MaxValue; i++) 
+		{
+			// Allocate edge reference array on nodes
+			nodes[i].Edges = new byte[8];
+
+			// Allocate node references and point arrays on edges
+			edges[i].Nodes = new byte[2];
+			edges[i].Points = new Vector2[2];
+		}
+
+		// Set function pointers/delegates
+		_parseFns = new _parseLineFn[] { ParseLineNodes, ParseLineEdges };
+		
+		LoadMap("test.txt");
     }
 
 	private void LoadMap(string filePath) 
 	{
 		// Open file stream
-		StreamReader reader = new StreamReader(filePath);
+		StreamReader reader = new StreamReader($"Assets/StreamingAssets/{filePath}");
 		if(reader == null) 
 		{
 			Debug.LogError($"ERROR: file: {filePath} does not exist");
@@ -66,7 +78,7 @@ public class MapLoader : MonoBehaviour
 		while((line = reader.ReadLine()) != null) 
 		{
 			ParseLine(line, ref sector, ref currId);
-			Debug.Log(line);
+			//Debug.Log(line);
 		}
 
 		// Close file stream
@@ -75,23 +87,29 @@ public class MapLoader : MonoBehaviour
 
 	private void ParseLine(string line, ref sbyte sector, ref byte currId) 
 	{
+		if(line.Length == 0 || line[0] == '#') return;
+
 		if(line[0] == '[') 
 		{
 			sector++;
 			return;
 		}
 
-		int eq = line.IndexOf('=');
+		int eq = -1;
+		eq = line.IndexOf('=');
 		if(eq == -1) return;
 
-		string key = line.Substring(0, eq);
-		string val = line.Substring(eq, + 1);
+		string key = line.Substring(0, eq).Trim();
+		string val = line.Substring(eq + 1).Trim();
 		
-		parseFns[sector](key, val, ref currId);
+		_parseFns[sector](key, val, ref currId);
 	}
 
 	private void ParseLineNodes(string key, string val, ref byte currId)	
 	{
+		Debug.Log($"key: {key}");
+		Debug.Log($"val: {val}");
+		
 		switch(key) 
 		{
 			case "count": 
@@ -100,18 +118,19 @@ public class MapLoader : MonoBehaviour
 
 			case "id":
 				currId = byte.Parse(val);
+				Debug.Log($"currId: {currId}");
 				break;
 			
 			case "label":
-				nodes[currId].label = val;
+				nodes[currId].Label = val;
 				break;
 
 			case "backgound_id":
-				nodes[currId].background = byte.Parse(val);
+				nodes[currId].Background = byte.Parse(val);
 				break;
 
 			case "e":
-				nodes[currId].edges[nodes[currId].EdgeCount++] = byte.Parse(val);
+				nodes[currId].Edges[nodes[currId].EdgeCount++] = byte.Parse(val);
 				break;
 		}
 	}
@@ -129,30 +148,36 @@ public class MapLoader : MonoBehaviour
 				break;
 
 			case "node_id_a":
-				edges[currId].nodes[0] = byte.Parse(val);
+				edges[currId].Nodes[0] = byte.Parse(val);
 				break;
 
 			case "node_id_b":
-				edges[currId].nodes[1] = byte.Parse(val);
+				edges[currId].Nodes[1] = byte.Parse(val);
 				break;
 
 			case "pos_a":
-				edges[currId].points[0] = ParseVec2(val);
+				edges[currId].Points[0] = ParseVec2(val);
 				break;
 				
 			case "pos_b":
-				edges[currId].points[1] = ParseVec2(val);
+				edges[currId].Points[1] = ParseVec2(val);
 				break;
 		}
 	}
 
 	private Vector2 ParseVec2(string str)
 	{
-		int braceId = str.IndexOf('{');
+		Debug.Log($"parsing vec2: {str}");
+
+		int braceOpenId = str.IndexOf('{');
+		int BraceCloseId = str.IndexOf('}');
 		int commaId = str.IndexOf(',');	
 
-		string strFloatX = str.Substring(braceId, commaId);
-		string strFloatY = str.Substring(commaId + 2, str.Length - 1);
+		string strFloatX = str[(braceOpenId+1)..commaId];
+		string strFloatY = str[(commaId+2)..BraceCloseId];
+
+		Debug.Log($"str_x: {strFloatX}");
+		Debug.Log($"str_y: {strFloatY}");
 
 		return new Vector2(float.Parse(strFloatX), float.Parse(strFloatY));
 	}
